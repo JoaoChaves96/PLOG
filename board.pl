7a7a60b0364fb929d94f21e0af698df018726db4
rpedro10 <rpedro10@gmail.com>
2016-10-20 21:22:45 +0100

    nova visualizacao
    nova visualizacao

--- board.pl
+++ board.pl
@@ -9,6 +9,79 @@ board([ [q,q,d,s],
 
 
 display_board_letter(B):-
+	write('    a   b   c   d'),
+	nl,
+	write('   --------------- '),
+	nl.
+
+
+half_board(N):- ( N=4 -> write('  =================');write('   ---------------') ).
+
+
+display_board([L1|Ls],X,Y,N):-
+	display_line_number(N),
+	N1 is N+1,
+	write('|'),
+	display_line(L1),
+	nl,
+	half_board(N),
+	nl,
+	display_board(Ls,X,Y,N1).
+
+display_board([],X,Y,_):-
+	nl,
+	write('player1: '),
+	write(X),
+	nl,
+	write('player2: '),
+	write(Y),
+	nl, nl.
+
+display_line_number(N):-
+	write(N),
+	write(' ').
+
+display_line([E|Es]):-
+	translate(E,V),
+	write(' '),
+	write(V),
+	write(' |'),
+	display_line(Es).
+	
+display_line([]).
+
+	
+translate(s,' ').
+translate(p,'*').
+translate(d, 'x').
+translate(q, 'X').
+
+read_player(X):-
+	write('player name: '),
+	read(X),
+	nl.
+
+play_game(B,X,Y):-
+	board(B),
+	read_player(X),
+	read_player(Y),
+	nl,
+	nl,
+	display_board_letter(B),
+	display_board(B,X,Y,1).
+
+/**
+board([ [q,q,d,s],
+		[q,d,p,s],
+		[d,p,p,s],
+		[s,s,s,s],
+		[s,s,s,s],
+		[s,p,p,d],
+		[s,p,d,q],
+		[s,d,q,q]]).
+
+
+display_board_letter(B):-
 	write('   a b c d'),
 	nl,
 	write('   - - - -'),
@@ -72,3 +145,4 @@ play_game(B,X,Y):-
 	nl,
 	display_board_letter(B),
 	display_board(B,X,Y,1).
+*/
